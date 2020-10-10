b9565a41aaf05a4a1c80a018f8897e88d5a94c71
Valeriy.C <valerii.chalenko@mail.ru>
2020-10-10 19:19:43 +0300

    init

---
+++ src/main/java/com/doitinjava/uselessapp/UselessappApplication.java
@@ -0,0 +1,13 @@
+package com.doitinjava.uselessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UselessappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UselessappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doitinjava/uselessapp/UselessappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doitinjava.uselessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UselessappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
